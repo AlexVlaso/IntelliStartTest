ea6496c61a27116e17ec54377dbbaa1ff7b24f4a
AlexVlaso <109801450+AlexVlaso@users.noreply.github.com>
2022-07-23 11:04:37 +0300

    Fix users object

--- src/main/java/market/Main.java
+++ src/main/java/market/Main.java
@@ -9,9 +9,9 @@ public class Main {
         Product product2 = new Product(2,"laptop",1200);
         Product product3 = new Product(3,"TV",750);
 
-        User user1 = new User(1,"Vlasov","Alex",3000);
-        User user2 = new User(2,"Ivanov","Sergey",1500);
-        User user3 = new User(3,"Ponomarenko","Max",2000);
+        User user1 = new User(1,"Alex","Vlasov",3000);
+        User user2 = new User(2,"Sergey","Ivanov",1500);
+        User user3 = new User(3,"Max","Ponomarenko",2000);
 
     }
 }
